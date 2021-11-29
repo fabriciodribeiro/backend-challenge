d5d6450a3e200b28ae3dc5833754d2cfea7ae17f
Fabricio Ribeiro <fabriciodribeiro@hotmail.com>
2021-11-29 22:30:39 +0000

    Project reorganization

---
+++ src/Challenge.Core/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Challenge.Core.Models
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Challenge.Core/Models/Portfolio.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+
+namespace Challenge.Core.Models
+{
+    public class Portfolio : BaseEntity
+    {
+        public DateTime Date { get; private set; }
+
+        public Portfolio(string name)
+        {
+            Date = DateTime.UtcNow;
+        }
+    }
+}
